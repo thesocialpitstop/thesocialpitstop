38d62159d8b499ac27e5a43551a098a73befa8d1
Justin Peng <justinpeng13@gmail.com>
2022-05-19 23:41:04 +0800

    Set up Terraform (S3 and Cloudfront)

--- thesocialpitstop-terraform/thesocialpitstop.tf
+++ thesocialpitstop-terraform/thesocialpitstop.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "TheSocialPitstop"
+
+    workspaces {
+      name = "prod"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,12 +15,89 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
+# Variables
+variable "access_key" {
+  description = "AWS IAM access key"
+  type        = string
+  sensitive   = true
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+variable "secret_key" {
+  description = "AWS IAM secret key"
+  type        = string
+  sensitive   = true
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# S3
+resource "aws_s3_bucket" "b" {
+  bucket = "the-social-pitstop"
+
+  tags = {
+    Name        = "TheSocialPitstop"
+    Environment = "production"
+  }
+}
+
+resource "aws_s3_bucket_acl" "b_acl" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+# Cloudfront
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    # s3_origin_config {
+    #   origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    # }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
