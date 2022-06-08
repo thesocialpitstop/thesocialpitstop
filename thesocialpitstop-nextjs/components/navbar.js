c9d10cbacdcf339189b32cef9aa901b7861fbb6e
Yue Hern <yuehernkang@hotmail.com>
2022-06-08 08:36:18 +0800

    added ory auth

--- thesocialpitstop-nextjs/components/navbar.js
+++ thesocialpitstop-nextjs/components/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Nav,
   NavbarContainer,
@@ -11,12 +11,53 @@ import {
 import { FaBars } from "react-icons/fa";
 import { BsUpload, BsSearch } from "react-icons/bs";
 import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0";
+import { Button } from "@mui/material";
+import { Configuration, V0alpha2Api } from "@ory/kratos-client";
+import { edgeConfig } from "@ory/integrations/next";
 
 const Navbar = ({ toggle }) => {
   const menuId = "main-menu";
-  const { user, error, isLoading } = useUser();
-  const link = user ? "/api/auth/logout" : "/api/auth/login";
+  // Contains the current session or undefined.
+  const [session, setSession] = useState();
+
+  // The URL we can use to log out.
+  const [logoutUrl, setLogoutUrl] = useState('');
+
+  // The error message or undefined.
+  const [error, setError] = useState();
+  const kratos = new V0alpha2Api(new Configuration(edgeConfig))
+
+  useEffect(() => {
+    // If the session or error have been loaded, do nothing.
+    if (session || error) {
+      return
+    }
+
+    // Try to load the session.
+    kratos
+      .toSession()
+      .then(({ data: session }) => {
+        // Session loaded successfully! Let's set it.
+        setSession(session)
+
+        // Since we have a session, we can also get the logout URL.
+        return kratos
+          .createSelfServiceLogoutFlowUrlForBrowsers()
+          .then(({ data }) => {
+            setLogoutUrl(data.logout_url)
+          })
+      })
+      .catch((err) => {
+        // An error occurred while loading the session or fetching
+        // the logout URL. Let's show that!
+        console.log(err);
+        setError({
+          error: err.toString(),
+          data: err.response?.data
+        })
+      })
+  }, [session, error])
+
   return (
     <>
       <Nav>
@@ -31,9 +72,10 @@ const Navbar = ({ toggle }) => {
             </Link>
             <Link href="/about-us" passHref>
               <NavLinkItem>ABOUT US</NavLinkItem>
-            </Link>                        
-            <Link href={link} passHref>
-              {user ? <NavLinkItem>LOGOUT</NavLinkItem> : <NavLinkItem>LOGIN</NavLinkItem>}
+            </Link>        
+            {/* <Button onClick={clickFunction}>Button</Button> */}
+            <Link href={session ? logoutUrl : '/api/.ory/self-service/login/browser'} passHref>
+              {session ? <NavLinkItem>LOGOUT</NavLinkItem> : <NavLinkItem>LOGIN</NavLinkItem>}
             </Link>
           </NavBtn>
         </NavbarContainer>
