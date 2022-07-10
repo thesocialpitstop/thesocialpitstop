923f68757fd0bef2cbc9bef1236e3bc9d8949ba3
Yue Hern <yuehernkang@hotmail.com>
2022-07-10 19:28:34 +0800

    ready to push

--- thesocialpitstop-nextjs/pages/profile/create.js
+++ thesocialpitstop-nextjs/pages/profile/create.js
@@ -1,127 +1,185 @@
 // import React from 'react';
 // import Link from 'next/link';
 // import Image from 'next/image';
-import { useState } from "react";
-import { Button, Switch, TextField, FormGroup, RadioGroup, Radio, FormControlLabel, MenuItem } from "@mui/material";
-import categories from "../../constants/categories";
-import { CREATE_PROFILE } from '../../graphql/mutations';
 import { useMutation } from "@apollo/client";
-import { useRouter } from 'next/router'
-import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
-
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useFormik } from "formik";
+import { parsePhoneNumber } from "libphonenumber-js";
+import { useRouter } from "next/router";
+import categories from "../../constants/categories";
+import { CREATE_PROFILE } from "../../graphql/mutations";
+import { profileCreateValidationSchema } from "./create_validation_schema";
 const CreateProfile = () => {
-  const [orgName, setOrgName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [orgType, setOrgType] = useState("");
-  const [category, setCategory] = useState("");
-  const [details, setDetails] = useState("");
-  
-  function parseJwt(token) {
-    if (!token) { return; }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
-  }
-
-  const { user } = useUser();
-  const router = useRouter();
-  const user_id = parseJwt(router.query.session_token)?.sub.split('|')[1];
-
-  const [createProfile] = useMutation(CREATE_PROFILE);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(orgName, orgType, category, email, phone, address);
-    createProfile({
-      variables: {
-        address: address,
-        category: category,
-        datetime: new Date().toISOString(),
-        details: details,
-        email: email,
-        item_type: `${orgType}-PROFILE`,
-        name: orgName,
-        user_id: user_id ? user_id : user?.sub.split('|')[1],
-        contact_num: phone
-      }
-    })
-    if (user_id) {
-      window.location = `https://the-social-pitstop.us.auth0.com/continue?state=${router.query.state}`;
-    } else {
-      router.push("/dashboard");
+  const parseJwt = (token) => {
+    if (!token) {
+      return;
     }
-  }
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+    return JSON.parse(window.atob(base64));
+  };
+  const router = useRouter();
+  const user_id = parseJwt(router.query.session_token)?.sub.split("|")[1];
+  const [createProfile] = useMutation(CREATE_PROFILE);
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      org_type: "SOO",
+      category: "",
+      email: "",
+      phone: '',
+      details: "",
+      address: "",
+    },
+    validationSchema: profileCreateValidationSchema,
+    onSubmit: async (values) => {
+      console.log(values);
+      createProfile({
+        variables: {
+          address: values.address,
+          category: values.category,
+          datetime: new Date().toISOString(),
+          details: values.details,
+          email: values.email,
+          item_type: `${values.org_type}-PROFILE`,
+          name: values.name,
+          user_id: user_id ? user_id : user?.sub.split("|")[1],
+          contact_num: parsePhoneNumber(`+65${values.phone}`, 'SG').number,
+        },
+        onCompleted: (data) => {
+          console.log("success");
+          if (user_id) {
+            window.location = `https://the-social-pitstop.us.auth0.com/continue?state=${router.query.state}`;
+          } else {
+            router.push("/dashboard");
+          }
+        },
+        onError: (error) => {
+          console.error("error", error);
+        }
+      });
+
+    },
+  });
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <h2>Create your profile !</h2>
+      <form onSubmit={formik.handleSubmit}>
         <FormGroup row>
           <TextField
-            required
+            margin="normal"
+            fullWidth
             id="name"
             label="Organisation Name"
-            onInput={e => setOrgName(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.name}
           />
-          <RadioGroup
-            row
-            name="org_type"
-            required
-            onChange={e => setOrgType(e.target.value)}
-          >
-            <FormControlLabel value="SOO" control={<Radio />} label="SOO" />
-            <FormControlLabel value="BIZ" control={<Radio />} label="Business" />
-          </RadioGroup>
+        </FormGroup>
+        <RadioGroup
+          margin="normal"
+          row
+          name="org_type"
+          id="org_type"
+          onChange={formik.handleChange}
+          value={formik.values.org_type}
+        >
+          <FormControlLabel value="SOO" control={<Radio />} label="SOO" />
+          <FormControlLabel value="BIZ" control={<Radio />} label="Business" />
+        </RadioGroup>
+        <FormGroup>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+            <Select
+              labelId="category-select"
+              id="category"
+              name="category"
+              value={formik.values.category}
+              label="Category"
+              onChange={formik.handleChange}
+            >
+              {categories.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* <TextField
+              margin="normal"
+              id="category"
+              select
+              label="Category"
+              onChange={formik.handleChange}
+              value={formik.values.category}
+              >
+              {categories.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </TextField> */}
         </FormGroup>
 
         <FormGroup>
           <TextField
-            id="category"
-            select
-            label="Category"
-            onChange={e => setCategory(e.target.value)}
-          // helperText="
-          >
-            {categories.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </FormGroup>
-
-        <FormGroup>
-          <TextField
+            margin="normal"
             id="email"
             label="Email"
             type="email"
-            onInput={e => setEmail(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.email}
           />
           <TextField
+            margin="normal"
             id="phone"
+            name="phone"
             label="Contact No."
-            onInput={e => setPhone(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.phone}
           />
           <TextField
+            margin="normal"
             id="address"
+            name="address"
             label="Address"
-            onInput={e => setAddress(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.address}
           />
         </FormGroup>
 
-        <TextField
-          id="details"
-          label="Details"
-          helperText="Tell us about your organisation"
-          multiline
-          onInput={e => setDetails(e.target.value)}
-        />
-
-        <Button type="submit" variant="contained">Create</Button>
+        <FormGroup>
+          <TextField
+            margin="normal"
+            id="details"
+            rows={4}
+            label="Details"
+            multiline
+            onChange={formik.handleChange}
+            value={formik.values.details}
+          />
+        </FormGroup>
+        <Button
+          type="submit"
+          disabled={!(formik.isValid && formik.dirty && !formik.isSubmitting)}
+          variant="contained"
+        >
+          Create
+        </Button>
       </form>
     </>
   );
-}
+};
 
 export default CreateProfile;
