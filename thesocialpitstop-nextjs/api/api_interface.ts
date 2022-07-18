6d45337bb0a35a4c838284dee2cc9ec46aa6cb0a
Yue Hern <yuehernkang@hotmail.com>
2022-07-19 06:13:06 +0800

    added api interface ts

--- thesocialpitstop-nextjs/api/api_interface.ts
+++ thesocialpitstop-nextjs/api/api_interface.ts
@@ -1,4 +1,15 @@
+import { useQuery } from '@apollo/client';
+import { GET_PROFILE } from '../graphql/queries';
+
 export class APIInterface {
-    
+    getProfile(user_id: string) {
+        const { data: profile } = useQuery(GET_PROFILE, {
+            variables: {
+            user_id: user_id,
+            item_type: "SOO-PROFILE",
+            },
+        });
+        return profile;
+    }
 }
 
